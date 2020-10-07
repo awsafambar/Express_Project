1e68ad8d6e6c04ca07836050c5da3ab7e2854bf6
awsafambar <awsafamber@gmail.com>
2020-10-08 01:44:18 +0530

    Passport

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,24 +34,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 //app.use(cookieParser('12345-67890-09876-54321'));
 app.use(session({name: 'session-id', secret: '12345-67890-09876-54321', saveUninitialized: false, resave: false, store: new FileStore()}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-    console.log(req.session);
+function auth (req, res, next) {
+    console.log(req.user);
 
-    if (!req.session.user) {
-        var err = new Error('You are not authenticated!');
-        err.status = 403;
-        return next(err);
-    } else {
-        if (req.session.user === 'authenticated') {
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
+    if (!req.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      next(err);
+    }
+    else {
+          next();
     }
 }
 
